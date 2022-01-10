a5ee9c861dc62b96579f14f9a505b9e6da7ddbd2
chenshuoran <3362065779@qq.com>
2022-01-10 17:20:01 +0800

    1

--- src/twoSum_001/Solution.java
+++ src/twoSum_001/Solution.java
@@ -1,4 +1,12 @@
 package twoSum_001;
 
-public class Solution {
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int []result=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i+nums[j]==target])
+            }
+        }
+    }
 }
